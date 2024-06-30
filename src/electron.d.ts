b54988b6bede13865129dc67746fdf628c788dfa
danik-progger <lyatorovskiy@gmail.com>
2024-06-30 11:56:14 +0300

    first commit

---
+++ src/electron.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="vite/client" />
+/// <reference types="electron" />
+
+declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string;
+declare const MAIN_WINDOW_VITE_NAME: string;
+
+type Clipping = {
+    id: string;
+    type?: string;
+    value: string;
+};
+
+declare interface Window {
+    api: import("./preload").Api;
+}
